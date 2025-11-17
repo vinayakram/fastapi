2393c4583d1e9585087f44e79232ffe510cca536
vinayakram <vinayakram.r@prodapt.com>
2025-11-17 16:48:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,12 @@ async def root():
 @app.get("/health")
 async def health():
   return {"status": "ok"}
+
+
+@app.get("/add")
+async def add(x : int, y : int):
+    return { "result" : xvalue+yvalue }
+
+@app.get("/multiply")
+async def multiply(x : int, y: int):
+    return { "result" : x*y }
