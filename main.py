ea797228947c5cccb47c592331a8191be63ff8dd
Vinayak Ram R <vinayakram.r@prodapt.com>
2025-11-17 14:41:46 +0530

    adding root message

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+async def root():
+    return {"status": "hello world"}
+
 @app.get("/health")
 async def health():
   return {"status": "ok"}
