21fe3d54eda4e486becbe9e0d07ff2bc1eade755
vinayakram <vinayakram1@gmail.com>
2025-11-19 06:41:30 +0530

    updates

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 
 app = FastAPI()
+
+jobboards = { "acme" : [{"title": "customer support executive","job description":"L3"},{"title":"project manager","job description":"manage a team of 2"}],
+              "pcg" : [{"title": "technical architect","job description":"solution architect"},{"title":"junior software engineer","job description":"code in c"}],
+              "atlas" : [{"title": "lead engineer","job description":"lead a team of 2"}]
+              }
+              
 
 @app.get("/")
 async def root():
     return {"status": "hello world deploy"}
+    
+    
 
 @app.get("/health")
 async def health():
@@ -18,3 +26,14 @@ async def add(x : int, y : int):
 @app.get("/multiply")
 async def multiply(x : int, y: int):
     return { "result" : x*y }
+    
+    
+@app.get("/job-boards/{q}")
+async def jobboard(q: str):
+        if q not in jobboards:
+            raise HTTPException(
+                status_code=404,
+                detail="Company not listed",
+                headers={"Company not listed": "Error"}
+                )
+        return jobboards[q]
